65af6f7311f294af3f4b509f1f694e193a610d51
Kyra Kadhim <kyrakadhim@gmail.com>
2024-03-01 16:48:08 +0100

    Commenting cleanup and docstring revision

--- jaxley_mech/synapses/ribbon.py
+++ jaxley_mech/synapses/ribbon.py
@@ -7,28 +7,43 @@ class RibbonSynapse(Synapse):
     Compute synaptic current and update synapse state for a deterministic ribbon
     synapse.
 
-    Ribbon synapse from Schroeder et al. 2019 supplemented by the full synapse model of
+    Ribbon synapse from Schroeder et al. 2020 supplemented by the full synapse model of
     the Dayan & Abbott 2001 Theoretical Neuroscience textbook. A single exponential
-    decay is used to model the postsynaptic conductance, also referred to as the
-    probability that a postsynaptic channel opens given that transmitter was released.
+    decay is used to model postsynaptic conductance.
+
+    synapse_params:
+        gS: Maximal synaptic conductance (S)
+        tau: Decay time constant of postsynaptic conductance (s)
+        e_syn: Reversal potential of postsynaptic membrane at the receptor (mV)
+        lam: Vesicle replenishment rate at the ribbon
+        p_r: Probability of a vesicle at the ribbon moving to the dock
+        D_max: Maximum number of docked vesicles
+        R_max: Maximum number of vesicles at the ribbon
+
+    synapse_states:
+        released: Number of vesicles released
+        docked: Number of vesicles at the dock
+        ribboned: Number of vesicles at the ribbon
+        P_rel: Normalized vesicle release
+        P_s: Kernel of postsynaptic conductance
     """
 
-    synapse_params = {"gS": 0.5, "tau": 0.5, "e_syn": 0, "lam": 0.4, "p_r": 0.1}
+    synapse_params = {
+        "gS": 0.5,
+        "tau": 0.5,
+        "e_syn": 0,
+        "lam": 0.4,
+        "p_r": 0.1,
+        "D_max": 8,
+        "R_max": 50,
+    }
     synapse_states = {"released": 0, "docked": 4, "ribboned": 25, "P_rel": 0, "P_s": 0}
 
     def update_states(self, u, delta_t, pre_voltage, post_voltage, params):
         """Return updated synapse state."""
 
-        R_max = 50
-        D_max = 8
-
         k = 1.0
         V_half = -35
-        p_r = params["p_r"]
-        λ = params["lam"]
-        ρ = 0.35
-
-        P_max = 1.0
 
         # Presynaptic voltage to calcium to release probability
         p_d_t = 1 / (1 + jnp.exp(-k * (pre_voltage - V_half)))
@@ -37,16 +52,15 @@ class RibbonSynapse(Synapse):
         new_released = p_d_t * u["docked"]
 
         # Movement to the dock
-        new_docked = u["docked"] + p_r * u["ribboned"] - new_released
-        new_docked = jnp.maximum(new_docked, D_max)
+        new_docked = u["docked"] + params["p_r"] * u["ribboned"] - new_released
+        new_docked = jnp.maximum(new_docked, params["D_max"])
 
         # Movement to the ribbon
-        new_ribboned = u["ribboned"] + λ - new_docked
-        new_ribboned = jnp.maximum(new_ribboned, R_max)
+        new_ribboned = u["ribboned"] + params["lam"] - new_docked
+        new_ribboned = jnp.maximum(new_ribboned, params["R_max"])
 
-        # Vesicle released to "transmitter release probability"
-        P_rel = new_released / D_max
-        P_s = P_max * jnp.exp(-delta_t / params["tau"])
+        P_rel = new_released / params["D_max"]
+        P_s = jnp.exp(-delta_t / params["tau"])
 
         return {
             "released": new_released,
