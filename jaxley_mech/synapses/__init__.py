88b009486c91e57a5add2b3acc8d2edda6916b41
Cornelius Schroeder <cornelius.schroeder@uni-tuebingen.de>
2024-04-29 09:04:08 +0200

    fixed merge conlict in __init__

--- jaxley_mech/synapses/__init__.py
+++ jaxley_mech/synapses/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-from .dms98 import AMPA, NMDA, GABAa, GABAb
+from .dms98kai import AMPA, NMDA, GABAa, GABAb
 from .gap_junction import GapJunction
-=======
-from . import dms98
-from .dms98 import AMPA, NMDA, GABAa, GABAb
->>>>>>> Stashed changes
 from .ribbon import RibbonSynapse
