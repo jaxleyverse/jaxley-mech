4382db97ef11e95013722b16bfd904f6b084d35a
michaeldeistler <michael.deistler95@gmail.com>
2024-05-24 16:31:46 +0200

    add `init_states` to CaPump

--- jaxley_mech/channels/fm97.py
+++ jaxley_mech/channels/fm97.py
@@ -369,6 +369,10 @@ class CaPump(Channel):
     def compute_current(self, states, v, params):
         """The pump does not directly contribute to the membrane current."""
         return 0
+    
+    def init_state(self, voltages, params):
+        """Initialize the state at fixed point of gate dynamics."""
+        return {}
 
 
 class KCa(Channel):
